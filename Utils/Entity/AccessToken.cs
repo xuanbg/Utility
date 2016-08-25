e4098b5f3117c29341680c997424b6fa44b71dce
xan.brian <xuan_bingan@hotmail.com>
2016-08-25 17:05:52 +0800

    增加基类：AccessToken

---
+++ Utils/Entity/AccessToken.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Insight.Utils.Entity
+{
+    /// <summary>
+    /// 用户会话信息
+    /// </summary>
+    public class AccessToken
+    {
+        /// <summary>
+        /// 自增ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 用户类型
+        /// </summary>
+        public int UserType { get; set; }
+
+        /// <summary>
+        /// 用户OpenId
+        /// </summary>
+        public string OpenId { get; set; }
+
+        /// <summary>
+        /// 用户账号
+        /// </summary>
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 用户签名，用户名（大写）+ 密码MD5值的结果的MD5值
+        /// 仅用于获取Token
+        /// </summary>
+        public string Signature { get; set; }
+
+        /// <summary>
+        /// 登录用户ID
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// 登录用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 登录部门ID
+        /// </summary>
+        public Guid? DeptId { get; set; }
+
+        /// <summary>
+        /// 登录部门全称
+        /// </summary>
+        public string DeptName { get; set; }
+
+        /// <summary>
+        /// 用户机器码
+        /// </summary>
+        public string MachineId { get; set; }
+
+        /// <summary>
+        /// 随机密码
+        /// </summary>
+        public string Secret { get; set; }
+
+        /// <summary>
+        /// Secret失效时间
+        /// </summary>
+        public DateTime FailureTime { get; set; }
+    }
+}
--- Utils/Entity/Session.cs
+++ Utils/Entity/Session.cs
@@ -5,62 +5,12 @@ namespace Insight.Utils.Entity
     /// <summary>
     /// 用户会话信息
     /// </summary>
-    public class Session
+    public class Session:AccessToken
     {
-        /// <summary>
-        /// 自增ID
-        /// </summary>
-        public int ID { get; set; }
-
-        /// <summary>
-        /// 登录用户ID
-        /// </summary>
-        public Guid UserId { get; set; }
-
-        /// <summary>
-        /// 登录用户名
-        /// </summary>
-        public string UserName { get; set; }
-
-        /// <summary>
-        /// 用户OpenId
-        /// </summary>
-        public string OpenId { get; set; }
-
-        /// <summary>
-        /// 用户账号
-        /// </summary>
-        public string LoginName { get; set; }
-
-        /// <summary>
-        /// 用户签名，用户名（大写）+ 密码MD5值的结果的MD5值
-        /// </summary>
-        public string Signature { get; set; }
-
-        /// <summary>
-        /// 登录部门ID
-        /// </summary>
-        public Guid? DeptId { get; set; }
-
-        /// <summary>
-        /// 登录部门全称
-        /// </summary>
-        public string DeptName { get; set; }
-
-        /// <summary>
-        /// 用户类型
-        /// </summary>
-        public int UserType { get; set; }
-
         /// <summary>
         /// 用户状态
         /// </summary>
         public bool Validity { get; set; }
-
-        /// <summary>
-        /// 用户机器码
-        /// </summary>
-        public string MachineId { get; set; }
 
         /// <summary>
         /// 连续失败次数
@@ -81,7 +31,6 @@ namespace Insight.Utils.Entity
         /// 用户登录结果
         /// </summary>
         public LoginResult LoginResult { get; set; }
-
     }
 
     /// <summary>
