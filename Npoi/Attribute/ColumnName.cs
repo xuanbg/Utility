b56ac8a34a2fdf641a808c2ac512e2c0e756dd13
Brian <xan.brian@gmail.com>
2018-08-02 18:10:48 +0800

    up

--- Npoi/Attribute/ColumnName.cs
+++ Npoi/Attribute/ColumnName.cs
@@ -38,5 +38,40 @@ namespace Insight.Utils.ExcelHelper.Attribute
         {
             this.policy = policy;
         }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="name">属性/字段名称</param>
+        /// <param name="dateFormat">时间/日期格式</param>
+        public ColumnName(string name, string dateFormat)
+        {
+            this.name = name;
+            this.dateFormat = dateFormat;
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="name">属性/字段名称</param>
+        /// <param name="policy">导入/导出策略</param>
+        public ColumnName(string name, Policy policy)
+        {
+            this.name = name;
+            this.policy = policy;
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="name">属性/字段名称</param>
+        /// <param name="dateFormat">时间/日期格式</param>
+        /// <param name="policy">导入/导出策略</param>
+        public ColumnName(string name, string dateFormat, Policy policy)
+        {
+            this.name = name;
+            this.policy = policy;
+            this.dateFormat = dateFormat;
+        }
     }
 }
