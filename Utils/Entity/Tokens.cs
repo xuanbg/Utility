8c9d8b6fbf64a1afbe69a00c7b6aee1db1ed49d9
Brian <xan.brian@gmail.com>
2018-09-01 15:41:27 +0800

    up

--- Utils/Entity/Tokens.cs
+++ Utils/Entity/Tokens.cs
@@ -1,4 +1,6 @@
-﻿namespace Insight.Utils.Entity
+﻿using System;
+
+namespace Insight.Utils.Entity
 {
     public class TokenPackage
     {
@@ -49,16 +51,6 @@
         public string id { get; set; }
 
         /// <summary>
-        /// 租户ID
-        /// </summary>
-        public string tenantId { get; set; }
-
-        /// <summary>
-        /// 登录部门ID
-        /// </summary>
-        public string deptId { get; set; }
-
-        /// <summary>
         /// 姓名/昵称
         /// </summary>
         public string name { get; set; }
@@ -79,8 +71,53 @@
         public string email { get; set; }
 
         /// <summary>
+        /// 租户ID
+        /// </summary>
+        public string tenantId { get; set; }
+
+        /// <summary>
+        /// 租户名称
+        /// </summary>
+        public string tenantName { get; set; }
+
+        /// <summary>
+        /// 登录部门ID
+        /// </summary>
+        public string deptId { get; set; }
+
+        /// <summary>
+        /// 登录部门编码
+        /// </summary>
+        public string deptCode { get; set; }
+
+        /// <summary>
+        /// 登录部门名称
+        /// </summary>
+        public string deptName { get; set; }
+
+        /// <summary>
         /// 描述
         /// </summary>
         public string remark { get; set; }
+
+        /// <summary>
+        /// 是否预置：0、自定；1、预置
+        /// </summary>
+        public bool isBuiltin { get; set; }
+
+        /// <summary>
+        /// 是否失效：0、有效；1、失效
+        /// </summary>
+        public bool isInvalid { get; set; }
+
+        /// <summary>
+        /// 创建人ID
+        /// </summary>
+        public string creatorId { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime createTime { get; set; }
     }
 }
